42e5769deef4fc677b2f49913181e17eed330b5f
Billy Coleman <30822729-ItsBuddha@users.noreply.replit.com>
2024-07-29 03:43:01 +0000

    resolved import error

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Header from "../components/Header";
 import TodoInput from "../components/TodoInput";
 import TodoList from "../components/TodoList";
 import Footer from "../components/Footer";
+import { motion } from "framer-motion";
 
 interface Task {
   id: number;
